c91057a3ed1a11dba1d62da25c4ee2bba173c2b8
Lei Jing <leijing@Leis-MacBook-Pro.local>
2016-10-05 15:52:29 +0800

    added input

--- wx/pages/todos/todos.js
+++ wx/pages/todos/todos.js
@@ -11,15 +11,18 @@ function findDiffArr(a1, a2) {
 
 Page({
   data: {
-    todos: []
+    todos: [],
+    client: null,
+    inputValue: '',
+    isInserting: false
   },
   onLoad: function () {
     const Asteroid = asteroid.createClass()
-    const client = new Asteroid({
+    this.data.client = new Asteroid({
       endpoint: "ws://localhost:3000/websocket"
     });
-    client.subscribe("tasks");
-    client.ddp.on("added", ({collection, id, fields}) => {
+    this.data.client.subscribe("tasks");
+    this.data.client.ddp.on("added", ({collection, id, fields}) => {
       const contains = underscore.find(todoArr, (td) => id === td.id)
       if (!contains){
         fields.id = id
@@ -29,7 +32,7 @@ Page({
         todos: todoArr.sort((a,b) => b.createdAt.$date - a.createdAt.$date)
       })
     });
-    client.ddp.on("changed", ({collection, id, fields}) => {
+    this.data.client.ddp.on("changed", ({collection, id, fields}) => {
       const idx = underscore.findIndex(todoArr, (td) => id === td.id)
       if (idx>=0){
         todoArr[idx].checked = fields.checked
@@ -38,7 +41,7 @@ Page({
         todos: todoArr.sort((a,b) => b.createdAt.$date - a.createdAt.$date)
       })
     });
-    client.ddp.on("removed", ({collection, id}) => {
+    this.data.client.ddp.on("removed", ({collection, id}) => {
       const idx = underscore.findIndex(todoArr, (td) => id === td.id)
       if (idx>=0){
         todoArr.splice(idx, 1)
@@ -53,15 +56,36 @@ Page({
     let diffId = findDiffArr(underscore.pluck(checkedArr, 'id'), e.detail.value)[0]
     const diffItem = todoArr.find(t => t.id == diffId)
     const Asteroid = asteroid.createClass()
-    const client = new Asteroid({
-      endpoint: "ws://localhost:3000/websocket"
-    });
-    client.call("tasks.setChecked", diffId, !diffItem.checked)
+    
+    this.data.client.call("tasks.setChecked", diffId, !diffItem.checked)
       .then(result => {
           console.log("Success");
       })
       .catch(error => {
           console.error(error);
       });
-  }
+  },
+  bindKeyInput: function(e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  bindAddBtnTap: function() {
+    //sometime this'd be called multi times at one tap
+    if(!underscore.pluck(this.data.todos, 'text').includes(this.data.inputValue) && !this.data.isInserting){
+      console.log(this.data.inputValue)
+      this.data.isInserting = true
+      this.data.client.call("tasks.insert", this.data.inputValue)
+        .then(result => {
+          console.log("Success");
+          this.data.isInserting = false
+        })
+        .catch(error => {
+          console.error(error);
+          this.data.isInserting = false
+        });
+    } else {
+      console.log("can not insert same text")
+    }
+  },
 })
